998c5e980f9790bb78c9263c252a481abfa4742f
gxing <gxing19@163.com>
2018-06-04 12:43:26 +0800

    delete all file,modify folder

--- src/main/java/com/springboot/example/profile/ImportXMLFile.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.example.profile;
-
-//加载XML配置
-//@ImportResource(locations =  {"classpath:some-context.xml","classpath:another-context.xml"})
-@SuppressWarnings("unused")
-public class ImportXMLFile {
-
-}
