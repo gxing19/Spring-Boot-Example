0bca42d4931f4f27bb0a7398c46fb80b5de2d22e
gxing <gxing19@163.com>
2018-07-23 15:16:33 +0800

    embed tomcat ssl setting

--- spring-boot-web/src/main/java/com/springboot/web/Application.java
+++ spring-boot-web/src/main/java/com/springboot/web/Application.java
@@ -1,7 +1,13 @@
 package com.springboot.web;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +15,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+
 }
