a1abe59df542a20c9c52fcf43da4861f4fb3ce7e
guangxing.zheng <guangxing.zheng@qizhidao.com>
2019-12-14 16:01:39 +0800

    define transaction aop

--- spring-boot-datasource/src/main/java/com/springboot/datasource/DatasourceApplication.java
+++ spring-boot-datasource/src/main/java/com/springboot/datasource/DatasourceApplication.java
@@ -1,13 +1,10 @@
 package com.springboot.datasource;
 
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 
-// TransactionAutoConfiguration.class
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, TransactionAutoConfiguration.class})
 public class DatasourceApplication {
 
