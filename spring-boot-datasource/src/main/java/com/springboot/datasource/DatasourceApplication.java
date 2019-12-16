b3dddd17326ae0b17309be9c901804b2ab1ebc65
guangxing.zheng <guangxing.zheng@qizhidao.com>
2019-12-16 15:47:59 +0800

    exclude DataSourceTransactionManagerAutoConfiguration

--- spring-boot-datasource/src/main/java/com/springboot/datasource/DatasourceApplication.java
+++ spring-boot-datasource/src/main/java/com/springboot/datasource/DatasourceApplication.java
@@ -3,9 +3,9 @@ package com.springboot.datasource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, TransactionAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class DatasourceApplication {
 
     public static void main(String[] args) {
