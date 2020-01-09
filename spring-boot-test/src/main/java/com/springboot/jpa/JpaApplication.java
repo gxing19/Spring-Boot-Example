7c44b279528ca6060d0ce814721619ef1e59e81f
gxing <gxing19@163.com>
2020-01-09 13:25:52 +0800

    test

--- spring-boot-test/src/main/java/com/springboot/jpa/JpaApplication.java
+++ spring-boot-test/src/main/java/com/springboot/jpa/JpaApplication.java
@@ -1,12 +1,19 @@
 package com.springboot.jpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JpaApplication {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
     }
+
 }
---
+++ spring-boot-test/src/main/java/com/springboot/jpa/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.springboot.jpa.common.config;
+
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplateBuilder().setConnectTimeout(Duration.ofMillis(1000)).setReadTimeout(Duration.ofMillis(1000)).build();
+    }
+}
