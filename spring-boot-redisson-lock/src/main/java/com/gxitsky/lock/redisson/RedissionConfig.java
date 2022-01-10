86b7bc3d72555f30882e63ae56717a48fd7fe3fc
郑光星 <zhengguangxing@clearofchina.com>
2022-01-10 19:36:18 +0800

    修改配置

--- spring-boot-redisson-lock/src/main/java/com/gxitsky/lock/redisson/RedissionConfig.java
+++ spring-boot-redisson-lock/src/main/java/com/gxitsky/lock/redisson/RedissionConfig.java
@@ -17,7 +17,8 @@ public class RedissionConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setTimeout(1000000).setAddress("redis://192.168.0.120:6379");
+//        config.useSingleServer().setTimeout(5000).setAddress("redis://192.168.0.120:6379");
+        config.useSingleServer().setTimeout(5000).setAddress("redis://192.168.50.187:6379");
         return Redisson.create(config);
     }
 }
