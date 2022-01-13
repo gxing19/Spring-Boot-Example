eaa8d911429e2dfcc2a1461fd76c25ddfbd91e18
郑光星 <zhengguangxing@clearofchina.com>
2022-01-13 11:09:45 +0800

    add captcha example

--- spring-boot-captcha/src/test/java/com/gxitsky/CaptchaApplicationTests.java
+++ spring-boot-captcha/src/test/java/com/gxitsky/CaptchaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootCaptchaApplicationTests {
+class CaptchaApplicationTests {
 
     @Test
     void contextLoads() {
