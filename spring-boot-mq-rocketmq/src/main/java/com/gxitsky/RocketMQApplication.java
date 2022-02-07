27ee0ca4a305f25e5ce8ac602134c392987587b9
gxing19 <gxing19@163.com>
2022-02-07 14:05:07 +0800

    update import

--- spring-boot-mq-rocketmq/src/main/java/com/gxitsky/RocketMQApplication.java
+++ spring-boot-mq-rocketmq/src/main/java/com/gxitsky/RocketMQApplication.java
@@ -1,5 +1,6 @@
 package com.gxitsky;
 
+import org.apache.rocketmq.common.message.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class RocketMQApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RocketMQApplication.class, args);
+        Message message = new Message();
     }
 
 }
