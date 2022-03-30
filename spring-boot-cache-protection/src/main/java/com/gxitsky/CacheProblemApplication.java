69a3c46a2fd5190ea02e454cd1f4df1e1054e41f
gxing19 <gxing19@163.com>
2022-03-30 18:02:22 +0800

    update cache protection

---
+++ spring-boot-cache-protection/src/main/java/com/gxitsky/CacheProblemApplication.java
@@ -0,0 +1,13 @@
+package com.gxitsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheProblemApplication.class, args);
+    }
+
+}
